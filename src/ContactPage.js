53ed5168470a4e576d1b890d2ae1d1a9b96d9562
Aslam Abbas <108674067+aslamwebsite@users.noreply.github.com>
2024-07-16 18:26:48 +0530

    design

--- src/ContactPage.js
+++ src/ContactPage.js
@@ -6,20 +6,25 @@ import BreadCrumb from './component/BreadCrumb/Index'
 import Container from './component/Container/Index'
 
 const ContactPage = () => {
-    const getStrengthValue = () => {
-        const isIOS = /iPad|iPhone|iPod/.test(navigator.platform);
-        const isMac = /MacIntel/.test(navigator.platform);
-        return isIOS || isMac ? 100 : 300;
-    };
+  const getStrengthValue = () => {
+    const isIOS = /iPad|iPhone|iPod/.test(navigator.platform);
+    const isMac = /MacIntel/.test(navigator.platform);
+    return isIOS || isMac ? 100 : 300;
+  };
   return (
     <>
-        <Parallax bgImage={bannerImage} strength={getStrengthValue()} className="flex-center col-12 float-start parallaxBanner" />
-        <Container _parentClass={`m-0`}>  <BreadCrumb pageName={'Contact Us'}/></Container>
-    <div className="section proDetail">
-                <Contact />
-              </div>
-    </>
-  )
+      <Parallax bgImage={bannerImage} strength={getStrengthValue()} className="flex-center col-12 float-start parallaxBanner">
+        <div className='container position-relative'>
+        <div className="creativeslide">
+        <h3 className="heading bigFont text-start text-black">EXPECT <span>Exceptional</span></h3></div>
+        </div>
+        </Parallax>
+      <Container _parentClass={`m-0`}> <BreadCrumb pageName={'Contact Us'} /></Container>
+        <div className="section proDetail">
+          <Contact />
+        </div>
+      </>
+        )
 }
 
-export default ContactPage
+        export default ContactPage
