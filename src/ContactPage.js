fac6ade1b02badd618799ead665b5f4704e15bf7
Aslam Abbas <108674067+aslamwebsite@users.noreply.github.com>
2024-07-18 13:51:51 +0530

    design

--- src/ContactPage.js
+++ src/ContactPage.js
@@ -1,30 +1,71 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import { Parallax } from 'react-parallax';
-import bannerImage from './images/contactBanner.webp'
-import Contact from './component/Contact/Index'
-import BreadCrumb from './component/BreadCrumb/Index'
-import Container from './component/Container/Index'
+import Contact from './component/Contact/Index';
+import BreadCrumb from './component/BreadCrumb/Index';
+import Container from './component/Container/Index';
+import { BasePath } from './component/BasePath/Index';
 
 const ContactPage = () => {
+  const [pageData, setpageData] = useState(null);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`${BasePath}/contact_details.php`);
+        setpageData(response.data);
+        console.log(response.data);
+      } 
+     
+      catch (error) {
+        setError(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+  if (isLoading) {
+    return <div className="preloader"></div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
   const getStrengthValue = () => {
     const isIOS = /iPad|iPhone|iPod/.test(navigator.platform);
     const isMac = /MacIntel/.test(navigator.platform);
     return isIOS || isMac ? 100 : 300;
   };
+
+
   return (
     <>
-      <Parallax bgImage={bannerImage} strength={getStrengthValue()} className="flex-center col-12 float-start parallaxBanner">
-        <div className='container position-relative'>
-        <div className="creativeslide">
-        <h3 className="heading bigFont text-start text-black">EXPECT <span>Exceptional</span></h3></div>
+      <Parallax
+       bgImage={pageData.bannerImage[0].imagePath}
+        strength={getStrengthValue()}
+        className="flex-center col-12 float-start parallaxBanner"
+      >
+        <div className="container position-relative">
+          <div className="creativeslide">
+            <h3 className="heading bigFont text-start text-black">
+              EXPECT <span>Exceptional</span>
+            </h3>
+          </div>
         </div>
-        </Parallax>
-      <Container _parentClass={`m-0`}> <BreadCrumb pageName={'Contact Us'} /></Container>
-        <div className="section proDetail">
-          <Contact />
-        </div>
-      </>
-        )
-}
+      </Parallax>
+      <Container _parentClass="m-0">
+        <BreadCrumb pageName="Contact Us" />
+      </Container>
+      <div className="section proDetail">
+        <Contact />
+      </div>
+    </>
+  );
+};
 
-        export default ContactPage
+export default ContactPage;
--- src/component/Tabs/Tabs.js
+++ src/component/Tabs/Tabs.js
@@ -1,36 +1,45 @@
 import React from 'react';
-import BreadCrubs from '../BreadCrumb/Index'
+import BreadCrumb from '../BreadCrumb/Index';
 
 const Tabs = ({ titles, onClick, activeIndex, Slug, Category, Whitelogo, BredCat }) => {
-   return (
+  const sentenceCaseTitle = (title) => {
+    if (!title) return '';
+    return title.charAt(0).toUpperCase() + title.slice(1).toLowerCase();
+  };
+
+  return (
     <>
-    <div className="fixed-strip projectDetails">
-        <div className='container'>
-          <div className='row'>
-            <div className='flex-center col-12'>
-              <div className='column3'>
-                  <BreadCrubs pageName={BredCat} pageUrl={'/projects/' + BredCat.toLowerCase()} _parentClass={'marginRemoveul'} pageChildName={Slug}/>
+      <div className="fixed-strip projectDetails">
+        <div className="container">
+          <div className="row">
+            <div className="flex-center col-12">
+              <div className="column3">
+                <BreadCrumb
+                  pageName={BredCat}
+                  pageUrl={'/projects/' + BredCat.toLowerCase()}
+                  _parentClass={'marginRemoveul'}
+                  pageChildName={Slug}
+                />
               </div>
-              <div className='column9 justify-content-end fixMenu d-flex'>
-              {titles.map((title, index) => (
-        <div
-          key={index}
-          className={`title-item ${activeIndex === index ? 'active' : ''}`}
-          onClick={() => onClick(index)}
-        >
-          {title}
-        </div>
-      ))}
+              <div className="column9 justify-content-end fixMenu d-flex">
+                {titles.map((title, index) => (
+                  <div
+                    key={index}
+                    className={`title-item ${activeIndex === index ? 'active' : ''}`}
+                    onClick={() => onClick(index)}
+                  >
+                    {sentenceCaseTitle(title)}
+                  </div>
+                ))}
               </div>
             </div>
           </div>
         </div>
-    </div>
-    <div className='prologoHeader'>
-      <a><img src={Whitelogo} alt=''/></a>
-    </div>
+      </div>
+      <div className="prologoHeader">
+        <a><img src={Whitelogo} alt="" /></a>
+      </div>
     </>
-
   );
 };
 
