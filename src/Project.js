8be98a18e6b5948875aeb070fd2330fbc3247957
Akashtriverse1252 <akash.triverse@gmail.com>
2024-07-26 17:24:38 +0530

    adding link or whats next's

--- src/Project.js
+++ src/Project.js
@@ -1,12 +1,12 @@
-import React, { useEffect, useState } from 'react';
-import WebContainer from './component/WebContainer/Index';
-import Title from './component/Title/Index';
-import { useParams } from 'react-router-dom';
-import axios from 'axios';
-import { BasePath } from './component/BasePath/Index';
-import Noimage from './images/noimage.jpg';
-import BreadCrumb from './component/BreadCrumb/Index';
-import Container from './component/Container/Index';
+import React, { useEffect, useState } from "react";
+import WebContainer from "./component/WebContainer/Index";
+import Title from "./component/Title/Index";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import { BasePath } from "./component/BasePath/Index";
+import Noimage from "./images/noimage.jpg";
+import BreadCrumb from "./component/BreadCrumb/Index";
+import Container from "./component/Container/Index";
 
 const Project = () => {
   const [pageData, setPageData] = useState({});
@@ -14,13 +14,13 @@ const Project = () => {
   const [isLoading, setIsLoading] = useState(true);
   const { cat } = useParams();
   const location = window.location.href;
-  const pathSegments = location.split('/');
+  const pathSegments = location.split("/");
 
   useEffect(() => {
-    const validCategories = ['townships', 'homes', 'commercial'];
+    const validCategories = ["townships", "homes", "commercial"];
 
     if (!validCategories.includes(cat.toLowerCase())) {
-      setError(new Error('Invalid category'));
+      setError(new Error("Invalid category"));
       setIsLoading(false);
       return;
     }
@@ -43,9 +43,10 @@ const Project = () => {
   const handleScroll = (sectionId) => {
     const section = document.getElementById(sectionId);
     if (section) {
-      const yOffset = -50; 
-      const yPosition = section.getBoundingClientRect().top + window.pageYOffset + yOffset;
-      window.scrollTo({ top: yPosition, behavior: 'smooth' });
+      const yOffset = -50;
+      const yPosition =
+        section.getBoundingClientRect().top + window.pageYOffset + yOffset;
+      window.scrollTo({ top: yPosition, behavior: "smooth" });
     }
   };
 
@@ -54,41 +55,41 @@ const Project = () => {
   }
 
   if (error) {
-    return <div>Error: {error.message || 'Something went wrong!'}</div>;
+    return <div>Error: {error.message || "Something went wrong!"}</div>;
   }
 
   let title;
   switch (cat.toLowerCase()) {
-    case 'townships':
-      title = 'Townships';
+    case "townships":
+      title = "WHAT’S NEXT";
       break;
-    case 'homes':
-      title = 'Homes';
+    case "homes":
+      title = "Homes";
       break;
-    case 'commercial':
-      title = 'Commercial Projects';
+    case "commercial":
+      title = "Commercial Projects";
       break;
     default:
-      title = 'Projects';
+      title = "Projects";
   }
 
   const sections = {
-    MixedDevelopment: 'Mixed Development',
-    Retail: 'Retail',
-    Offices: 'Offices',
-    HotelApartments: 'Hotel Apartments'
+    MixedDevelopment: "Mixed Development",
+    Retail: "Retail",
+    Offices: "Offices",
+    HotelApartments: "Hotel Apartments",
   };
 
   return (
     <>
-      <Container _parentClass={'m-0'}>
+      <Container _parentClass={"m-0"}>
         <BreadCrumb pageName={title} />
       </Container>
-      <WebContainer _parentClass={'projects p-100 pt-0'}>
-        <Title firstHeading={title} grandClass={'m-0'} />
-        {cat.toLowerCase() === 'commercial' && (
-          <div className='col-12 float-start commercialProject desktop-show'>
-            <div className='tabs'>
+      <WebContainer _parentClass={"projects p-100 pt-0"}>
+        <Title firstHeading={title} grandClass={"m-0"} />
+        {cat.toLowerCase() === "commercial" && (
+          <div className="col-12 float-start commercialProject desktop-show">
+            <div className="tabs">
               <ul className="flex-center">
                 {Object.entries(sections).map(([sectionKey, sectionTitle]) => (
                   <li key={sectionKey} onClick={() => handleScroll(sectionKey)}>
@@ -99,20 +100,32 @@ const Project = () => {
             </div>
           </div>
         )}
-        {cat.toLowerCase() === 'commercial' ? (
+        {cat.toLowerCase() === "commercial" ? (
           <>
             {Object.entries(sections).map(([sectionKey, sectionTitle]) => (
-              <div id={sectionKey} key={sectionKey} className={'tabSection col-12 float-start p-100 pb-0'}>
-                <Title firstHeading={sectionTitle} grandClass={'m-0'} />
+              <div
+                id={sectionKey}
+                key={sectionKey}
+                className={"tabSection col-12 float-start p-100 pb-0"}
+              >
+                <Title firstHeading={sectionTitle} grandClass={"m-0"} />
                 <div className="col-12 float-start categoryimage flex-center gap-25 flex-wrap">
                   {pageData[sectionKey] && pageData[sectionKey].length > 0 ? (
                     pageData[sectionKey].map((project) => (
                       <div
-                        className={`projectslider ${pageData[sectionKey].length > 1 ? 'col-lg-6' : 'col-lg-12'}`}
+                        className={`projectslider ${
+                          pageData[sectionKey].length > 1
+                            ? "col-lg-6"
+                            : "col-lg-12"
+                        }`}
                         key={project.id}
                       >
-                        {project.linkActive === '1' ? (
-                          <a href={`/projects/${pathSegments[4].toLowerCase()}/${project.slug}`}>
+                        {project.linkActive === "1" ? (
+                          <a
+                            href={`/projects/${pathSegments[4].toLowerCase()}/${
+                              project.slug
+                            }`}
+                          >
                             <figure className="snip0016">
                               {project.image ? (
                                 <img src={project.image} alt={project.name} />
@@ -122,7 +135,7 @@ const Project = () => {
                               <figcaption>
                                 <h3>{project.name}</h3>
                                 <p>{project.location}</p>
-                                <p className='desktop-show'>Know More</p>
+                                <p className="desktop-show">Know More</p>
                               </figcaption>
                             </figure>
                           </a>
@@ -151,8 +164,12 @@ const Project = () => {
             {pageData[cat] && pageData[cat].length > 0 ? (
               pageData[cat].map((project) => (
                 <div className="projectslider" key={project.id}>
-                  {project.linkActive === '1' ? (
-                    <a href={`/projects/${pathSegments[4].toLowerCase()}/${project.slug}`}>
+                  {project.linkActive === "1" ? (
+                    <a
+                      href={`/projects/${pathSegments[4].toLowerCase()}/${
+                        project.slug
+                      }`}
+                    >
                       <figure className="snip0016">
                         {project.image ? (
                           <img src={project.image} alt={project.name} />
@@ -162,7 +179,7 @@ const Project = () => {
                         <figcaption>
                           <h3>{project.name}</h3>
                           <p>{project.location}</p>
-                            <p className='desktop-show'>Know More</p>
+                          <p className="desktop-show">Know More</p>
                         </figcaption>
                       </figure>
                     </a>
